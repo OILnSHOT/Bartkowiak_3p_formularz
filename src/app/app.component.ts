dd0f91b6c8e157e14c6b832c13dbe512d6bc4770
OILnSHOT <olibartkowiak@gmail.com>
2022-04-07 01:00:54 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, VERSION } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  klient_dane = new FormGroup({
+    dane: new FormGroup({
+      Imie: new FormControl('', [
+        Validators.pattern('[a-zA-Z Ł]*'),
+        Validators.required,
+      ]),
+      Nazwisko: new FormControl(''),
+      email: new FormControl('', [Validators.email, Validators.required]),
+    }),
+
+    telefon: new FormGroup({
+      stacjonarny: new FormControl(''),
+      komorkowy: new FormControl(''),
+    }),
+  });
+  onSubmit() {
+    console.log(this.klient_dane);
+  }
+}
